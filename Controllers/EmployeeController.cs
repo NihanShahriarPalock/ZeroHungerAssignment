8062dc75f865a274ac71119401de35af057da0b9
Nihan Shahriar Palock <nihanshahriar000@gmail.com>
2023-03-28 21:54:54 +0600

    v1

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHungerAssignment.Database;
+
+namespace ZeroHungerAssignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Employees()
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            return View(db.Employees.ToList());
+        }
+        public ActionResult List()
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            var Employees = db.Employees.ToList();
+            return View(Employees);
+        }
+        [HttpGet]
+        public ActionResult AddEmployees()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployees(Employee model)
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            db.Employees.Add(model);
+            db.SaveChanges();
+            TempData["Msg"] = "Employee Added Successfully";
+            return RedirectToAction("List", "Employee");
+
+        }
+
+        public ActionResult EditEmployee(int Id)
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            var emp = (from e in db.Employees
+                      where e.Id == Id
+                      select e).SingleOrDefault();
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult EditEmployee(Employee upemp)
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            var exst = (from e in db.Employees
+                        where e.Id == upemp.Id
+                        select e).SingleOrDefault();
+            exst.Name = upemp.Name;
+            exst.Email = upemp.Email;
+            exst.Age = upemp.Age;
+            exst.Gender = upemp.Gender;
+
+            //db.Entry(exst).CurrentValues.SetValues(upstudent);
+            db.SaveChanges();
+
+
+            //db.Students.Remove(exst);
+            TempData["Msg"] = "Information Updated Successfully";
+            return RedirectToAction("List", "Employee");
+        }
+
+        public ActionResult DeleteEmployee(int id)
+        {
+            var employee = new Employee() { Id = id };
+
+            using (var context = new ZeroHungerAssignmentEntities3())
+            {
+                context.Employees.Attach(employee);
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+            TempData["Msg"] = "Employee Removed Successfully";
+            return RedirectToAction("List", "Employee");
+        }
+
+       
+
+    }
+}
---
+++ Controllers/RestaurantController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHungerAssignment.Database;
+
+namespace ZeroHungerAssignment.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult ResIndex()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult MakeAnOrder()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MakeAnOrder(OrderList model)
+        {
+
+            var db = new ZeroHungerAssignmentEntities3();
+            db.OrderLists.Add(new OrderList
+                { 
+                Order_Status = "Waiting", 
+                Order_Date=DateTime.Now
+
+            });
+            var request = db.OrderLists.SingleOrDefault();
+            if (request != null)
+                request.Order_Status = "Pending";
+
+            db.SaveChanges();
+            return RedirectToAction("List", "Restaurant");
+        }
+
+        public ActionResult Orders()
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            return View(db.OrderLists.ToList());
+        }
+        public ActionResult List()
+        {
+            var db = new ZeroHungerAssignmentEntities3();
+            var OrderLists = db.OrderLists.ToList();
+            return View(OrderLists);
+        }
+
+
+
+
+
+
+
+
+    }
+}
